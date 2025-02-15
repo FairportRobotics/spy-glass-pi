d3ed86317321ad31b80a0fc19cf5d1b2515ff009
Mike Silva <mike.a.silva@gmail.com>
2025-02-15 10:10:55 -0500

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,13 @@ if [$step == 1]; then
     sudo apt install python3-opencv python3-munkres
     sudo reboot
 elif [$step == 2]; then
-    imx500-package -i packerOut.zip -o output
+    imx500-package -i packerOut.zip -o .
+    cd ..
+    git clone https://github.com/raspberrypi/picamera2
+    cd picamera2
+    pip install -e .  --break-system-packages
+    cd examples/imx500
+    python imx500_object_detection_demo.py --model ../spy-glass-pi/network.rpk --fps 25 --bbox-normalization --ignore-dash-labels --bbox-order xy --labels ../spy-glass-pi/labels.txt>
 else
     echo "Invalid step"
 fi
