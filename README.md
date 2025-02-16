8c29e8725d9b581a35721ad6c9b1909a6e73a440
Mike Silva <mike.a.silva@gmail.com>
2025-02-16 07:00:45 -0500

    Minor changes

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Spy Glass PI
 
 `git clone https://github.com/FairportRobotics/spy-glass-pi`
+
 `cd spy-glass-pi`
+
 `chmod +x setup.sh`
+
 `./setup.sh`
--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 echo -n "Step: "
 read step
 
-if [$step == 1]; then
+if [$step == 1]; then 
     sudo apt update && sudo apt full-upgrade
     sudo apt install imx500-all imx500-tools
     sudo apt install python3-opencv python3-munkres
@@ -14,8 +14,8 @@ elif [$step == 2]; then
     git clone https://github.com/raspberrypi/picamera2
     cd picamera2
     pip install -e .  --break-system-packages
-    cd examples/imx500
-    python imx500_object_detection_demo.py --model ../spy-glass-pi/network.rpk --fps 25 --bbox-normalization --ignore-dash-labels --bbox-order xy --labels ../spy-glass-pi/labels.txt>
+    cd ~/spy-glass-pi
+    python imx500_object_detection_demo.py --model network.rpk --fps 25 --bbox-normalization --ignore-dash-labels --bbox-order xy --labels labels.txt
 else
     echo "Invalid step"
 fi
